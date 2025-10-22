a564ae5b866787972433a126f8ebe1a82763c5b4
Harshal <harshal.madgulkar725@gmail.com>
2025-10-22 10:16:19 +0530

    Add sendVerificationCode utility for email verification

--- controller/authController.js
+++ controller/authController.js
@@ -2,7 +2,7 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddlewares.js";
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
-import crypto from "crypto";
+import { sendVerificationCode } from "../utils/sendVerificationCode.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
 	try {
---
+++ utils/sendVerificationCode.js
@@ -0,0 +1,19 @@
+export async function sendVerificationCode(verificationCode, email, res) {
+	try {
+		const message = generateVerificationOtpTemplate(verificationCode);
+		sendEmail({
+			email,
+			subject: "Verification Code (Bookworm Library Management System)",
+			message,
+		});
+		res.status(200).json({
+			success: true,
+			message: "Verification code sent successfully.",
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: "Verification code failed to sent.",
+		});
+	}
+}
