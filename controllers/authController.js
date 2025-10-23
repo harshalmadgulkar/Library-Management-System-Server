4d4d0e2b75a0d01fed2f2f96c31e3c1172611e12
Harshal <harshal.madgulkar725@gmail.com>
2025-10-23 19:14:09 +0530

    feat(auth): implement forgot password functionality with reset token and email
    - Added `forgotPassword` controller in `authController.js`
      - Generates password reset token and sends reset email using `sendEmail`
      - Uses new `generateForgotPasswordEmailTemplate` for styled email content
    - Updated `userModel.js` to include `getResetPasswordToken` method
      - Creates hashed reset token with expiration time
    - Added `/forgotPassword` route in `authRouter.js`

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,8 @@ import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import { sendVerificationCode } from "../utils/sendVerificationCode.js";
 import { sendToken } from "../utils/sendToken.js";
+import { generateForgotPasswordEmailTemplate } from "../utils/emailTemplates.js";
+import { sendEmail } from "../utils/sendEmail.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
 	try {
@@ -136,4 +138,38 @@ export const getUser = catchAsyncErrors(async (req, res, next) => {
 		sucess: true,
 		user,
 	});
+});
+
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+	if (!req.body.email) {
+		return next(new ErrorHandler("Please provide email", 400));
+	}
+	const user = await User.findOne({
+		email: req.body.email,
+		accountVerified: true,
+	});
+	if (!user) {
+		return next(new ErrorHandler("User not found", 400));
+	}
+	const resetToken = await user.getResetPasswordToken();
+	await user.save({ validateBeforeSave: false });
+	const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+	const message = generateForgotPasswordEmailTemplate(resetPasswordUrl);
+
+	try {
+		await sendEmail({
+			email: user.email,
+			subject: "Bookworm Library Management System Password Recovery",
+			message,
+		});
+		res.status(200).json({
+			sucess: true,
+			message: `Email sent to ${user.email} successfully`,
+		});
+	} catch (error) {
+		user.resetPassword = undefined;
+		user.resetPasswordExpire = undefined;
+		await user.save({ validateBeforeSave: false });
+		return next(new ErrorHandler(error.message, 500));
+	}
 });
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+	forgotPassword,
 	getUser,
 	login,
 	logout,
@@ -15,5 +16,6 @@ router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/getUser", isAuthenticated, getUser);
+router.post("/forgotPassword", forgotPassword);
 
 export default router;
