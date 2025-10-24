ae38be941a0bef42e2d8db0036addd9758b44979
Harshal <harshal.madgulkar725@gmail.com>
2025-10-24 18:44:31 +0530

    feat(auth): add role-based authorization middleware
    - Added `isAuthorized` middleware in `authMiddleware.js`
      - Restricts access to specific routes based on user roles
      - Returns 403 error if user role is not permitted

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -20,3 +20,17 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
 	req.user = await User.findById(decoded.id);
 	next();
 });
+
+export const isAuthorized = (...roles) => {
+	return (req, res, next) => {
+		if (!roles.includes(req.user.role)) {
+			return next(
+				new ErrorHandler(
+					`User with this role (${req.user.role}) is not allowed to access this  resource`,
+					403
+				)
+			);
+		}
+		next();
+	};
+};
